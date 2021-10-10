c187e34341bff804dc40f35f4e5fbd040f6087cd
phuocpyn <phuocnhse140866@fpt.edu.vn>
2021-10-10 18:35:01 +0700

    add more test case in Advanced...

--- test/com/phuocnh/mathutil/core/AdvancedFactorialTest.java
+++ test/com/phuocnh/mathutil/core/AdvancedFactorialTest.java
@@ -28,7 +28,7 @@ public class AdvancedFactorialTest {
             {3, 6},
             {4, 24},
             {5, 120},
-            {6, 720},
+            {6, 7720},
         };
     }
     @Parameter(value = 0)
